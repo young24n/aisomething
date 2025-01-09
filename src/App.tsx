f322065bcb60e997ba5cc632a412d94595251569
young24n <won8560@gmail.com>
2025-01-09 14:23:05 +0900

    대화 내용 기억 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,27 @@ import { useState } from 'react'
 import model from './model'
 import './App.css'
 
-function App() {
-  const prompt = async () => {
-    const prompt = ""
-    const result = await model.generateContent([prompt]);
+const chat = model.startChat();
 
-    console.log(result.response.text());
+function App() {
+  const [prompt, setPrompt] = useState('');
+  const [result, setResult] = useState('');
+
+  const output = async () => {
+    const chatResult = await chat.sendMessage(prompt);
+    setResult(chatResult.response.text());
   }
 
   return (
     <>
       <div>
-        <button onClick={prompt}>test</button>
+        <p>내용 입력</p>
+        <input onChange={(e) => {
+          setPrompt(e.target.value);
+        }}></input>
+        <button onClick={output}>send</button>
+        <p>결과: </p>
+        <p>{result}</p>
       </div>
     </>
   )
