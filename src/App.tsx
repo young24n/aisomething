ae4a3d1adbd11193ac320fbf9cdae994d7f09846
young24n <won8560@gmail.com>
2025-01-09 13:50:07 +0900

    simple test

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import model from './model'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const prompt = async () => {
+    const prompt = ""
+    const result = await model.generateContent([prompt]);
+
+    console.log(result.response.text());
+  }
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button onClick={prompt}>test</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
